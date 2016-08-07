7500c221bff2bccef87e6798fa2335633bc5b5b1
frodrok <=>
2016-08-07 17:16:29 +0200

    Tried my hand at manipulating JQuery FullCalendar
    * Can add calendar event via large plus sign and a modal
    * I am not good at javascript

--- server/public/javascripts/hello.js
+++ server/public/javascripts/hello.js
@@ -13,12 +13,11 @@ function setupCalendar(calendarObject, events) {
         return {id: event.id, title: event.title, start: event.from}
     });
 
+    /* 2016-08-07T14:20:00 */
+
     if (events.length < 1) {
         console.log("no events recieved");
     }
-
-    var today = new Date()
-    console.log(today.toTimeString());
 
     calendarObject.fullCalendar({
         header: {
@@ -27,21 +26,37 @@ function setupCalendar(calendarObject, events) {
              /*right: 'month,basicWeek,basicDay'*/
             right: 'month,agendaWeek,agendaDay'
         },
-        weekends: false,
+        weekends: true,
         fixedWeekCount: false,
         selectable: true,
         selectHelper: true,
-        select: function(start, end, allDay) {
-            var title = prompt('Event Title:');
+        /* select: function(start, end, allDay) {
+
+            console.log("in select start: " + start.format('YYYY-MM-DD hh:mm'));
+            console.log("in select end: " + end.format('YYYY-MM-DD hh:mm'));
+
+            showNewEventWindowAndGetVariables(function(e) {
+                var eventTitle = e.title;
+                var fromValue = e.fromInput;
+                var toValue = e.toInput;
+                var selectedStart = moment(new Date(start)).format('YYYY-MM-DD');
+                /* var selectedEnd = moment(new Date(end)).format('YYYY-MM-DD');
+                var selectedEnd = end.format('YYYY-MM-DD');
+
+                var totalStart = selectedStart + "T" + fromValue + ":00";
+                var totalEnd = selectedEnd + "T" + toValue + ":00";
+                addEventToCalendar(calendarObject, eventTitle, totalStart, totalEnd, allDay);
+            });
+
             /*
              if title is enterd calendar will add title and event into fullCalendar.
              */
-            if (title)
+            /* if (title)
             {
                 calendarObject.fullCalendar('renderEvent',
                     {
                         title: title,
-                        start: start,
+                        start: moment(new Date(start)).format(),
                         end: end,
                         allDay: allDay
                     },
@@ -49,10 +64,52 @@ function setupCalendar(calendarObject, events) {
                 );
             }
             calendarObject.fullCalendar('unselect');
+        }, */
+        eventClick: function(event, jsEvent, view) {
+          console.log("event: " + event + " clicked!");
         },
-        defaultDate: '2016-08-06',
+        defaultDate: '2016-08-07',
         editable: true,
         eventLimit: true,
+        aspectRatio: 1.6,
         events: fixedEvents
     });
+}
+
+function addEventToCalendar(calendarObject, title, start, end, allDay) {
+    console.log("adding event: " + start + " " + end);
+    calendarObject.fullCalendar('renderEvent',
+        {
+            title: title,
+            start: start,
+            end: end,
+            allDay: false
+        },
+        true
+    );
+
+    calendarObject.fullCalendar('unselect');
+}
+
+function showNewEventWindowAndGetVariables(callback) {
+
+    /* var title = prompt('Event Title:', event.title, { buttons: { Ok: true, Cancel: false} }); */
+
+    var neweventmodal = $('#neweventmodal').modal('show');
+    var neweventform = $("#newEventForm");
+
+    $('button#saveEventButton').click(function(e) {
+        e.preventDefault();
+
+        var from = $("#newEventForm input[name='from']").val();
+        var to = $("#newEventForm input[name='to']").val();
+        var title = $("#newEventForm input[name='title']").val();
+
+        neweventmodal.modal('hide');
+        neweventform[0].reset();
+
+        callback({title: title, fromInput: from, toInput: to})
+
+    });
+
 }
